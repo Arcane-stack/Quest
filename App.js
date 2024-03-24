c8d350d23db8e1a175adf63b3b716ca488025d92
ARCANE <149970860+Arcane-stack@users.noreply.github.com>
2024-03-24 22:46:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Back from './components/Back';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Back/>
+    </>
+  );
+}
+
+export default App;
---
+++ Back.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Cssforback.css"
+// import bgimage from './bgimage.jpg';
+
+
+export default function Back() {
+  return (
+    <>
+  
+  <div className='bg'>
+    <img src='https://www.paraggupta.com/wp-content/uploads/20220402152158CUET-.jpg' alt='Backgroundimage' id="bgimage"/>
+            </div>
+    </>
+  )
+}
